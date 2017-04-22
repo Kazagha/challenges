e9d0afc35a9dafcd974322341ce043b2c2639bae
Kazagha <chrisg.local@gmail.com>
2017-04-22 16:54:42 +1000

    wordvalue: Basic calculation
    Implement the calculation of word values

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,9 +1,19 @@
 from data import DICTIONARY, LETTER_SCORES
+import operator
 
-word_list = []
+word_list = {}
 
 with open(DICTIONARY) as dict:
     for word in dict:
-        word_list.append(word.strip("\n"))
+        word_list[(word.strip('\n')).upper()] = 0
 
-print(word_list)
+for word in word_list:
+    value = 0
+    for char in word:
+        if(str.isalpha(char)):
+            value = value + LETTER_SCORES[char]
+    else:
+        word_list[word] = value
+
+word_list_sorted = sorted(word_list.items(), key=operator.itemgetter(1))
+print(word_list_sorted)
