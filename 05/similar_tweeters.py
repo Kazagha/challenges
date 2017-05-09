7a5ecf3a7b927225f88af3c99744e477fd3531a3
Kazagha <chrisg.local@gmail.com>
2017-05-09 21:07:31 +1000

    similar_tweets: Initial steps to tokenize tweets

--- 05/similar_tweeters.py
+++ 05/similar_tweeters.py
@@ -1,7 +1,26 @@
 import sys
+import usertweets
+import re
+
+TOKEN = re.compile(r'\b\w{2}\w+')
+
+def _tokenize(tweets):
+
+    tweet_text = ''.join(line for line in tweets)
+
+    print(TOKEN.findall(tweet_text))
+
+    # filtering out stop words, URLs, digits, punctuation, words that only occur once or are less than 3 characters (and/or other noise ...)
+
+    # Remove Punctuation, digits
+
+    pass
 
 def similar_tweeters(user1, user2):
-    pass
+    user1_tweets = usertweets.UserTweets(user1)
+    user2_tweets = usertweets.UserTweets(user2)
+
+    t = _tokenize(t.text for t in user1_tweets)
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
