20637938e4d4443685f0a98a2079782985ee4708
Kazagha <chrisg.local@gmail.com>
2017-05-11 20:20:16 +1000

    similar_tweets: Tokenize words, begin to remove noise

--- 05/similar_tweeters.py
+++ 05/similar_tweeters.py
@@ -1,26 +1,35 @@
 import sys
 import usertweets
 import re
+from collections import Counter
 
 TOKEN = re.compile(r'\b\w{2}\w+')
+VOID_WORDS = {'and','are','the','use','then','these','set','https','from','can','you','has','that','will','was'}
 
 def _tokenize(tweets):
 
-    tweet_text = ''.join(line for line in tweets)
+    tweet_text = ''.join(line for line in tweets).lower()
 
-    print(TOKEN.findall(tweet_text))
+    # Remove digits, punctuation and works with less than three letters
+    tweet_text = TOKEN.findall(tweet_text)
 
-    # filtering out stop words, URLs, digits, punctuation, words that only occur once or are less than 3 characters (and/or other noise ...)
+    # filtering out stop words, URLs, digits, punctuation,
+    # words that only occur once or are less than 3 characters (and/or other noise ...)
+    #print(Counter(tweet_text))
 
-    # Remove Punctuation, digits
+    # Remove words that occur only once
+    return _remove_low_count(tweet_text)
 
-    pass
+def _remove_low_count(tweet):
+    tweet_dict = Counter(tweet)
+    return ({word for word in tweet_dict if tweet_dict[word] > 1} - VOID_WORDS)
 
 def similar_tweeters(user1, user2):
     user1_tweets = usertweets.UserTweets(user1)
-    user2_tweets = usertweets.UserTweets(user2)
+    #user2_tweets = usertweets.UserTweets(user2)
 
     t = _tokenize(t.text for t in user1_tweets)
+    print(list(t))
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
