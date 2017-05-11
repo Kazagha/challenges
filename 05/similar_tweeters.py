6e9d814ef1300b9dfeef41e9a1be92ef17d4ed91
Kazagha <chrisg.local@gmail.com>
2017-05-11 21:17:47 +1000

    similar_tweets: Change order or functions
    Remove stop words before determining the most common words

--- 05/similar_tweeters.py
+++ 05/similar_tweeters.py
@@ -13,16 +13,19 @@ def _tokenize(tweets, STOP_WORDS):
     # Remove digits, punctuation and works with less than three letters
     tweet_text = TOKEN.findall(tweet_text)
 
+    # Remove stop words
+    tweet_text = (word for word in tweet_text if word not in STOP_WORDS)
+
     # Use the Counter library to determine how many times each word occurs
-    tweet_dict = Counter(tweet_text)
+    tweet_dict = Counter(tweet_text).most_common(10)
 
     # Remove words that occur only once
     # Remove any of the words in the STOP_WORD list
-    return {word for word in tweet_dict if tweet_dict[word] > 1}  - STOP_WORDS
+    return (word for word in tweet_dict)
 
 def _load_stop_words():
     with open('stop_words.txt') as words:
-        return {word.strip('\n') for word in words}
+        return list(word.strip('\n') for word in words)
 
 def similar_tweeters(user1, user2):
     STOP_WORDS = _load_stop_words()
@@ -34,9 +37,9 @@ def similar_tweeters(user1, user2):
     # Tokenize Tweets
     user1_tweets = _tokenize((t.text for t in user1_tweets), STOP_WORDS)
 
-    print(user1_tweets)
+    print(list(user1_tweets))
 
-    print(f'Word List Length {len(t)}')
+    #print(f'Word List Length {len(t)}')
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
