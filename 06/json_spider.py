1cf608c80327e44b3c22f5a42e427f18c68e6504
Kazagha <chrisg.local@gmail.com>
2017-06-04 15:35:15 +1000

    json_spider: Outline methods

--- 06/json_spider.py
+++ 06/json_spider.py
@@ -13,3 +13,18 @@ if __name__ == "__main__":
 
     # Determine the oldest release date of any verison
     print(min(data['releases'][release][0]['upload_time'] for release in data['releases']))
+
+    def _load_list(file_name):
+        pass
+
+    def _load_json(package_name):
+        pass
+
+    def _min_date(json):
+        pass
+
+    def loadFeed(file_name):
+        pass
+
+    def exportFeed(file_name):
+        pass
