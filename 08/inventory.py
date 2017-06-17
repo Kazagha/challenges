b2829eecd75d394ac7ab826e4b7b17383067adf9
Kazagha <chrisg.local@gmail.com>
2017-06-17 17:15:24 +1000

    inventory: Begin to fetch user input

---
+++ 08/inventory.py
@@ -0,0 +1,24 @@
+from item import Item
+
+if __name__ == "__main__":
+
+    room = Item(name='Lounge Room', value=0)
+    room.contents.append(Item('TV',value=4000, contents=[]))
+    room.contents.append(Item('Board Games',value=1000, contents=[Item('Dixit',value=250,contents=[])]))
+
+    room.print()
+    selected_room = room
+
+    while(True):
+        user_input = input('>>>  ').strip(' ')
+
+        if(user_input == 'print'):
+            selected_room.print()
+        elif(user_input == 'new'):
+            name = input('Item name: ')
+            value = input('Item value: ')
+            selected_room.contents.append(Item(name, value=int(value), contents=[]))
+        elif(user_input[0:2] == 'cd'):
+            print('Changing directory...')
+        elif(user_input == 'exit'):
+            break
