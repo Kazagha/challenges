7a114a3edf326bc537b17a821ebe18e541782c58
Kazagha <chrisg.local@gmail.com>
2017-06-20 20:47:42 +1000

    inventory: Check parent
    Check that the parent of the currently slected item exists before
    attempting to select it.

--- 08/inventory.py
+++ 08/inventory.py
@@ -3,25 +3,31 @@ from item import Item
 if __name__ == "__main__":
 
     room = Item(name='House', value=0)
-    room = Item(name='Lounge Room', value=0, contents=[])
+    room.add(Item(name='Lounge Room', value=0, contents=[]))
+    room = room.get_item('Lounge Room')
     room.add(Item('TV',value=4000, contents=[]))
     room.add(Item('Board Games',value=1000, contents=[Item('Dixit',value=250,contents=[])]))
+    room = room.parent
 
     room.print()
     selected_room = room
 
     while(True):
-        user_input = input('>>>  ').strip(' ')
+        #user_input = input(f'{selected_room.name} >>>  ').strip(' ')
+        user_input = input(f'>>>  ').strip(' ')
 
         if(user_input == 'print'):
             selected_room.print()
         elif(user_input == 'add'):
             name = input('Item name: ')
             value = input('Item value: ')
-            selected_room.contents.append(Item(name, value=int(value), contents=[]))
+            selected_room.add(Item(name, value=int(value), contents=[]))
         elif(user_input[0:2] == 'cd'):
             if(user_input[3:] == '..'):
-                selected_room = selected_room.parent
+                if (selected_room.parent != None):
+                    selected_room = selected_room.parent
+                else:
+                    print(f'{selected_room.name} has no parent')
             else:
                 selected_room = selected_room.get_item(user_input[3:])
         elif(user_input == 'exit'):
