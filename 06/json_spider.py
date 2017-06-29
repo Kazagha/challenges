641b4a589ab3263d17299aa6c0270027378f032f
Kazagha <chrisg.local@gmail.com>
2017-06-29 19:14:52 +1000

    json_spider: Expand to load files and export the feed

--- 06/json_spider.py
+++ 06/json_spider.py
@@ -1,12 +1,14 @@
 import json
-import sys
 import re
+import csv
 from urllib import request
 
 
 class Spider():
     PATTERN = re.compile("'.*'")
     error_file = 'error_log.txt'
+    export_file = ''
+    package_dict = {}
 
     def _load_list(self, file_name):
         with open(file_name) as f:
@@ -15,6 +17,8 @@ class Spider():
 
     def _load_json(self, package_name):
         url = f'https://pypi.python.org/pypi/{package_name}/json'
+
+        print(f'Loading {package_name}...')
 
         try:
             response = request.urlopen(url)
@@ -27,20 +31,46 @@ class Spider():
         try:
             return max(json['releases'][release][0]['upload_time'] for release in json['releases'])
         except Exception:
-            print('Exception:')
+            #print('Exception')
+            return '-'
 
     def load_feed(self, file_name):
         package_list = self._load_list(file_name)
-        package_dict = {package : self._min_date(self._load_json(package)) for (package) in package_list}
+        #self.package_dict = {package : self._min_date(self._load_json(package)) for (package) in package_list}
+        #print(list(package_list))
 
-        print(package_dict)
+        with open(f'{self.export_file}', 'w', newline='', encoding='utf8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['package','release date'])
+            writer.writeheader()
+
+            for package in package_list:
+                self.export_package(writer, [package, self._min_date(self._load_json(package))])
+
+    def set_export_file(self, file_name):
+        self.export_file = file_name
+
+        #with open(f'{file_name}', 'w', newline='', encoding='utf8') as csvfile:
+            #writer = csv.DictWriter(csvfile, fieldnames=['package','release date'])
+
+            #writer.writeheader()
+
+    def export_package(self, writer, package):
+        writer.writerow({'package' : package[0], 'release date': package[1]})
 
     def export_feed(self, file_name):
-        pass
+
+        with open(f'{file_name}', 'w', newline='', encoding='utf8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['package','release date'])
+
+            writer.writeheader()
+            for package in self.package_dict:
+                writer.writerow({'package':package, 'release date':self.package_dict[package]})
 
 if __name__ == "__main__":
     spi = Spider()
+    spi.set_export_file('export_feed.csv')
     spi.load_feed('packages-simple.html')
+    #spi.export_feed('package-dates.csv')
 
     #url = "https://pypi.python.org/pypi/Scrapy/json"
     #response = request.urlopen(url)
