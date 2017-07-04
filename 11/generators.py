de19fbb41c0192a2cc1bff842bfbe9b9f760b257
Kazagha <chrisg.local@gmail.com>
2017-07-04 20:45:41 +1000

    generators: Initial commit
     Implement gen_files function; fetches files from the directory
      specified in the pattern
    
     Implement gen_lines function; returns each line of the files specified

---
+++ 11/generators.py
@@ -0,0 +1,42 @@
+import glob
+
+"""
+Turn the following unix pipeline into Python code using generators
+
+$ for i in ../*/*py; do grep ^import $i|sed 's/import //g' ; done | sort | uniq -c | sort -nr
+   4 unittest
+   4 sys
+   3 re
+   3 csv
+   2 tweepy
+   2 random
+   2 os
+   2 json
+   2 itertools
+   1 time
+   1 datetime
+"""
+
+def gen_files(pat):
+    for file_name in glob.glob(pat):
+        yield file_name
+
+def gen_lines(files):
+    for file_name in files:
+        with open(file_name) as text:
+            for line in text:
+                yield line
+
+
+def gen_grep(lines, pattern):
+    pass
+
+def gen_count(lines):
+    pass
+
+
+if __name__ == "__main__":
+    # call the generators, passing one to the other
+    files = gen_files('../*/*.py')
+    lines = gen_lines(files)
+
