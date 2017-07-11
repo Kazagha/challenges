58330bc2deb92eac4b11a979244a397ef361cd38
Kazagha <chrisg.local@gmail.com>
2017-07-11 20:26:02 +1000

    directors: Implement the 'get_movies_by_director' method
    Read data from the CSV file and create a dictionary object, listing
     each director and their movie data

--- 13/directors.py
+++ 13/directors.py
@@ -8,12 +8,28 @@ MIN_YEAR = 1960
 
 Movie = namedtuple('Movie', 'title year score')
 
-
 def get_movies_by_director():
     '''Extracts all movies from csv and stores them in a dictionary
     where keys are directors, and values is a list of movies (named tuples)'''
-    pass
+    with open(MOVIE_DATA, 'r', newline='', encoding='utf8') as file:
 
+        # Create a list from the data in the CSV file
+        reader = csv.DictReader(file, delimiter=',')
+        csv_data = list([row['director_name'], row['movie_title'].strip(' ').strip(u'\xa0')
+            , row['title_year'], row['imdb_score']] for row in reader)
+
+        # Create a set of director names (set = unique names)
+        director_names = {name[0] for name in csv_data}
+        # Create an empty dictionary to hold the movie data
+        director_dict = {}
+
+        # Add each director to the dict along with their movie data
+        for name in sorted(director_names):
+            director_dict[name] = \
+                [Movie(title=data[1], year=data[2] ,score=data[3]) for data in csv_data if data[0] == name]
+
+        # Return the completed dict object
+        return director_dict
 
 def get_average_scores(directors):
     '''Filter directors with < MIN_MOVIES and calculate averge score'''
@@ -38,9 +54,11 @@ def main():
     '''This is a template, feel free to structure your code differently.
     We wrote some tests based on our solution: test_directors.py'''
     directors = get_movies_by_director()
-    directors = get_average_scores(directors)
-    print_results(directors)
+    #directors = get_average_scores(directors)
+    #print_results(directors)
 
+    for d in directors:
+        print(f'{d} - {directors[d]}')
 
 if __name__ == '__main__':
     main()
